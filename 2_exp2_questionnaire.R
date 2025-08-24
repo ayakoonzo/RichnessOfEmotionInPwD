f33ee487e9e0582ae9555c4657f2eaa8af9b0f2c
melonsode <3595109+melonsode@users.noreply.github.com>
2025-08-24 15:36:55 +0900

    Exp1: evaluator's data, ANOVA script, and result added
    In Exp1 directory, data, script, and result were added.

---
+++ 2_exp2_questionnaire.R
@@ -0,0 +1,1 @@
+exp2_questionnaire <- read.csv("2_exp2_questionnaire.csv", fileEncoding = "UTF-8")
--- Exp1/p-ketasuu.R
+++ Exp1/p-ketasuu.R
@@ -8,4 +8,4 @@ source(paste0(wd,"/anovakun_489.txt"))
 x1 <- x[,c(2:7)]
 x1
 head(x1)
-anovakun(x1, "sA", 6, geta=T, auto=T, tech=TRUE)
+anovakun(x1, "sA", 6, geta=T, auto=T, tech=TRUE, peta = T, cilmd = T )
